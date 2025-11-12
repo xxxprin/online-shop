4e7d1f16a56c5a17fac3557becb7bff0b9d9fa1f
xxxprin <hoodiks@vk.com>
2025-11-12 19:28:04 +0600

    Add files via upload

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  testMatch: ["**/tests/**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
